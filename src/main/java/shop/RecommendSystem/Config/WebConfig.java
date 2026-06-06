a582328efe062255a9cbed234fc584df006d55a2
jimin <coolcats6762@gmail.com>
2026-06-06 22:17:16 +0900

    - favicon 추가

--- src/main/java/shop/RecommendSystem/Config/WebConfig.java
+++ src/main/java/shop/RecommendSystem/Config/WebConfig.java
@@ -25,7 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:8080") // 허용할 도메인
+                // 허용할 도메인
+                .allowedOrigins(
+                        "http://localhost:*",
+                        "https://*.mekaive.com",
+                        "https://mekaive.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 인증 정보(쿠키, 인증 헤더)를 포함할지 여부
